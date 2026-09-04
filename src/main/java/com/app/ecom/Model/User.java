17bc46a2e16722093628e4b999ace7f1b327a9fd
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-04 08:25:37 -0700

    Entity Relationship: user & address mapping

--- src/main/java/com/app/ecom/Model/User.java
+++ src/main/java/com/app/ecom/Model/User.java
@@ -1,12 +1,13 @@
 package com.app.ecom.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -20,4 +21,13 @@ public class User {
     private String email;
     private String phone;
     private UserRole role = UserRole.CUSTOMER;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
 }
