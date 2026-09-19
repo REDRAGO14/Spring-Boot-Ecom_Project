e1e1faf3e8b35fdf7d7ca07f9f5014add6baba1b
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-19 00:06:52 -0700

    implemented CustomUserDetailService to change default in memory Detail manager into JDBC Detail manager

---
+++ src/main/java/com/app/ecom/Service/CustomUserDetailService.java
@@ -0,0 +1,20 @@
+package com.app.ecom.Service;
+
+import com.app.ecom.Repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        return userRepository.findByUsername(username).orElseThrow(()->new UsernameNotFoundException("Couldn't Found UserName!"));
+    }
+}
