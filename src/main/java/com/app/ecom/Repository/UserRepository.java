7b8dbb5bae7d80bb3a0f40d48e83890c5d6c14ff
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-19 00:08:15 -0700

    implemented findByUsername methode inside UserRepository to Select user using username which will be utilized by CustomUserDetailService

--- src/main/java/com/app/ecom/Repository/UserRepository.java
+++ src/main/java/com/app/ecom/Repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.app.ecom.Repository;
 
 import com.app.ecom.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
