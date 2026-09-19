97a646db164dfca228767f94597ada5cbe5d92a1
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-19 01:16:00 -0700

    refactor USER DTO respective to (USERNAME AND password )field in the users model

--- src/main/java/com/app/ecom/DTO/UserResponse.java
+++ src/main/java/com/app/ecom/DTO/UserResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 public class UserResponse {
     private Long id;
+    private String username;
     private String firstName;
     private String lastName;
     private String email;
