2c592407a6b556ac3a3c7f6540fadfdfd67f341f
REDRAGON <148379059+REDRAGO14@users.noreply.github.com>
2026-09-19 05:01:05 -0700

    implemented a mechanism to validate incoming token and if the token is valid set it to security context holder

--- src/main/java/com/app/ecom/Filter/JWTFilter.java
+++ src/main/java/com/app/ecom/Filter/JWTFilter.java
@@ -1,29 +1,51 @@
 package com.app.ecom.Filter;
 
+import com.app.ecom.Service.CustomUserDetailService;
 import com.app.ecom.Util.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
+    private final CustomUserDetailService customUserDetailService;
 
-    public JWTFilter(JWTUtil jwtUtil) {
+    public JWTFilter(JWTUtil jwtUtil, CustomUserDetailService customUserDetailService) {
         this.jwtUtil = jwtUtil;
+        this.customUserDetailService = customUserDetailService;
     }
-
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        // Skip this filter entirely when hitting authentication endpoints
+        return request.getServletPath().startsWith("/api/authentication/");
+    }
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
        String authHeader = request.getHeader("Authorization");
-       String token;
-       String username;
+       String token = null;
+       String username = null;
        if(authHeader!=null && authHeader.startsWith("Bearer ")){
             token = authHeader.substring(7);
             username = jwtUtil.extractUsername(token);
        }
+       if(username!=null&& SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+           if(jwtUtil.validateToken(username, userDetails,token)){
+               UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+               authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(authToken);
+           }
+       }
+        filterChain.doFilter(request, response);
     }
 }
